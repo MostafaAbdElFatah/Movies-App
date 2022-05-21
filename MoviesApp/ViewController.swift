a8f8f7683de98f91ff7fb19eed6334687d65fd2e
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2022-05-21 03:59:00 +0200

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Mostafa Abd ElFatah on 5/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
